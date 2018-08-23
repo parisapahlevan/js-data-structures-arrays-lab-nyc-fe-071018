a505c7996ab2833ea628cf1e2a0b1d7a6c37d58c
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-23 00:17:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendDriver(names){
+  
+  drivers.push(names);
+}
+function destructivelyPrependDriver(names){
+  
+  drivers.unshift(names);
+}
+function destructivelyRemoveLastDriver(names){
+  
+  drivers.pop(names);
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+ 
+function appendDriver(names){
+ const newArray= [ ...drivers, names]; 
+ return newArray;
+ }
+function prependDriver(names){
+ const newArray = [names, ...drivers];
+return newArray;
+}
+
+function removeLastDriver(){
+return drivers.slice(0, drivers.length - 1);
+ 
+ }
+ 
+ function removeFirstDriver(){
+   return drivers.slice(1);
+   
+ }
